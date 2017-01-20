7bc550e2246b678ff49810fd2486cf5154691c65
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2017-01-20 17:17:52 +0700

    update controller

--- CRUDJqueryDatatables/Controllers/HomeController.cs
+++ CRUDJqueryDatatables/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CRUDJqueryDatatables.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,92 @@ namespace CRUDJqueryDatatables.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Get
         public ActionResult Index()
         {
             return View();
         }
-
-       
+        public ActionResult GetEmployees()
+        {
+            using (EmployeeContext dc = new EmployeeContext())
+            {
+                var employees = dc.Employees.OrderBy(a => a.FirstName).ToList();
+                return Json(new { data = employees }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpGet]
+        public ActionResult Save(int id)
+        {
+            using (EmployeeContext dc = new EmployeeContext())
+            {
+                var v = dc.Employees.Where(a => a.EmployeeID == id).FirstOrDefault();
+                return View(v);
+            }
+        }
+        [HttpPost]
+        public ActionResult Save(Employee emp)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (EmployeeContext dc = new EmployeeContext())
+                {
+                    if (emp.EmployeeID > 0)
+                    {
+                        //Edit 
+                        var v = dc.Employees.Where(a => a.EmployeeID == emp.EmployeeID).FirstOrDefault();
+                        if (v != null)
+                        {
+                            v.FirstName = emp.FirstName;
+                            v.LastName = emp.LastName;
+                            v.EmailID = emp.EmailID;
+                            v.City = emp.City;
+                            v.Country = emp.Country;
+                        }
+                    }
+                    else
+                    {
+                        //Save
+                        dc.Employees.Add(emp);
+                    }
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            using (EmployeeContext dc = new EmployeeContext())
+            {
+                var v = dc.Employees.Where(a => a.EmployeeID == id).FirstOrDefault();
+                if (v != null)
+                {
+                    return View(v);
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteEmployee(int id)
+        {
+            bool status = false;
+            using (EmployeeContext dc = new EmployeeContext())
+            {
+                var v = dc.Employees.Where(a => a.EmployeeID == id).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Employees.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
     }
 }
